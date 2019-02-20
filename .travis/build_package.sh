b14fc2fca98e732b69b25dee1022ff59a5fc974b
Davide Gheri <davide.gheri@docomodigital.com>
2019-02-20 15:28:27 +0100

    travis tests

--- .travis/build_package.sh
+++ .travis/build_package.sh
@@ -1,9 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-set -e
-
-if git diff --name-only $1 | sort -u | uniq | grep $2; then
-    echo "To build $PACKAGE"
+if [[ -z $1 ]]; then
+    echo "Commit range cannot be empty"
+    exit 1
 fi
 
+if [[ -z $2 ]]; then
+    echo "Change path cannot be empty"
+    exit 1
+fi
 
+git diff --name-only $1 | sort -u | uniq | grep $2 > /dev/null
