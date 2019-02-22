a47109ad36ab8c8b559bf8a41f6e7b17a81a522a
Davide Gheri <davide.gheri@docomodigital.com>
2019-02-22 12:22:04 +0100

    js-fetcher

---
+++ packages/js-fetcher/src/index.browser.js
@@ -0,0 +1,8 @@
+import JsFetcher from './Fetcher';
+import TokenGenerator from './TokenGenerator';
+import * as utils from './utils';
+
+JsFetcher.TokenGenerator = TokenGenerator;
+JsFetcher.utils = utils;
+
+export default JsFetcher;
